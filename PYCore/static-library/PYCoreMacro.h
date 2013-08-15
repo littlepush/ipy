44da9e8bcfe44cd0d5612e068e74b5043a0536ca
Push Chen <littlepush+github@gmail.com>
2013-08-15 11:16:36 +0800

    fix a typing error in PYCore

--- PYCore/static-library/PYCoreMacro.h
+++ PYCore/static-library/PYCoreMacro.h
@@ -345,7 +345,7 @@ if ( !(condition) ) PYTHROW( f, ##__VA_ARGS__ )
 #define IS_WIDESCREEN (fabs((double)[[UIScreen mainScreen] bounds].size.height - \
                         (double)568) < DBL_EPSILON)
 #define IS_IPHONE ([[[UIDevice currentDevice] model] isEqualToString:@"iPhone"])
-#define IS_IPOD   (ß[[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
+#define IS_IPOD   ([[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
 
 #define IS_IPHONE_5 (IS_IPHONE && IS_WIDESCREEN)
 
